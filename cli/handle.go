4bc9bb0195f9e17d7a7808a0a1a21f31005938c5
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-04-12 13:12:12 +0200

    feat(cli): help

--- cli/handle.go
+++ cli/handle.go
@@ -1,6 +1,9 @@
 package cli
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 type FlagType uint
 
@@ -64,13 +67,46 @@ func (c *CLI) findCommand(name string) *Command {
 }
 
 func (c *CLI) basicHelp() {
-
+	println("RSS Goes Social - CLI Help")
+	println("==========================")
+	m := 0
+	for _, cmd := range c.Commands {
+		if m < len(cmd.Name) {
+			m = len(cmd.Name)
+		}
+	}
+	for _, cmd := range c.Commands {
+		sep := ""
+		for range -len(cmd.Name) {
+			sep += " "
+		}
+		fmt.Printf("\n%s%s - %s\n", cmd.Name, sep, cmd.Help)
+	}
 }
 
 func (c *CLI) commandHelp(cmd *Command) {
+	d := "RSS Goes Social - " + cmd.Name + " Help"
+	sep := ""
+	for range len(d) {
+		sep += "="
+	}
+	println(d + "\n" + sep)
 
+	m := 0
+	for _, f := range cmd.Flags {
+		if m < len(f.Name) {
+			m = len(f.Name)
+		}
+	}
+	for _, f := range cmd.Flags {
+		sep = ""
+		for range m - len(f.Name) {
+			sep += " "
+		}
+		fmt.Printf("%s%s - %s\n", f.Name, sep, f.Help)
+	}
 }
 
 func (c *CLI) invalidCommand() {
-
+	println("Invalid command. Check the help with rss-goes-social help or with rss-goes-social help {command}")
 }
