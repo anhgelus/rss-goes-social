a8c234f57a571de603fca23a9f5adb8b4cae91e3
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-04-12 14:30:00 +0200

    feat(cli): setup support flags -id and -secret

--- api/setup.go
+++ api/setup.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/anhgelus/rss-goes-social/utils"
 	"io"
@@ -52,6 +53,90 @@ func Setup() {
 		return
 	}
 	url := os.Args[2]
+
+	var clientID string
+	var clientSecret string
+	flag.StringVar(&clientID, "id", "", "client_id of the application (not required)")
+	flag.StringVar(&clientSecret, "secret", "", "client_secret of the application (not required)")
+	flag.Parse()
+
+	if len(clientID) != 0 && len(clientSecret) == 0 {
+		println("-id is set and not -secret. You have to set these two flags.")
+		return
+	} else if len(clientID) == 0 && len(clientSecret) != 0 {
+		println("-secret is set and not -id. You have to set these two flags.")
+		return
+	} else if len(clientID) == 0 && len(clientSecret) == 0 {
+		newApplication(url, &clientID, &clientSecret)
+	}
+
+	println("Now you have to click on the link below and login to your account. It will answer you if you " +
+		"really want to add this application. Click on 'Allow' and copy the token.")
+	fmt.Printf(utils.GetFullUrl(
+		url,
+		fmt.Sprintf(
+			"/oauth/authorize?%s=client_id&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code",
+			clientID),
+	) + "\n")
+	print("The token obtained from the link above -> ")
+	var tmpToken string
+	_, err := fmt.Scanln(&tmpToken)
+	if err != nil {
+		println("Error", "client_id: "+clientID, " and client_secret: "+clientSecret)
+		panic(err)
+	}
+	println("Getting the token.")
+	b, err := json.Marshal(setupRequestToken{
+		RedirectUris: RedirectUris,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		GrantType:    "authorization_code",
+		Code:         tmpToken,
+	})
+	if err != nil {
+		println("Error", "client_id: "+clientID, " and client_secret: "+clientSecret)
+		panic(err)
+	}
+	req, err := newRequestNoToken(http.MethodPost, utils.GetFullUrl(url, "/oauth/token"), strings.NewReader(string(b)))
+	if err != nil {
+		println("Error", "client_id: "+clientID, " and client_secret: "+clientSecret)
+		panic(err)
+	}
+	res, err := doRequest(req)
+	if err != nil {
+		println("Error", "client_id: "+clientID, " and client_secret: "+clientSecret)
+		panic(err)
+	}
+	b, err = io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	var tokenGot setupTokenGot
+	err = json.Unmarshal(b, &tokenGot)
+	if err != nil {
+		println("Error", "client_id: "+clientID, " and client_secret: "+clientSecret)
+		panic(err)
+	}
+	println("Verifying the token.")
+	err = VerifyToken(url, tokenGot.AccessToken)
+	if err != nil {
+		println(
+			"Error while verifying the validity of the token",
+			"client_id: "+clientID, " client_secret: "+clientSecret,
+			" token: "+tokenGot.AccessToken,
+		)
+		panic(err)
+	}
+	fmt.Printf("These information are important and sensible. Keep them secret and safe in the right place!\n"+
+		"Your token is: %s\nYour client_id is: %s\nYour client_secret is: %s",
+		tokenGot.AccessToken,
+		clientID,
+		clientSecret,
+	)
+}
+
+func newApplication(url string, clientID *string, clientSecret *string) {
+	println("Registering the application.")
 	b, err := json.Marshal(setupRegisterApplication{
 		ClientName:   AppName,
 		RedirectUris: RedirectUris,
@@ -81,64 +166,6 @@ func Setup() {
 	if err != nil {
 		panic(err)
 	}
-	println("Now you have to click on the link below and login to your account. It will answer you if you " +
-		"really want to add this application. Click on 'Allow' and copy the token.")
-	fmt.Printf(utils.GetFullUrl(
-		url,
-		fmt.Sprintf(
-			"/oauth/authorize?%s=client_id&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code",
-			appCreated.ClientID),
-	) + "\n")
-	var tmpToken string
-	_, err = fmt.Scanln(&tmpToken)
-	if err != nil {
-		println("Error", "client_id: "+appCreated.ClientID, " and client_secret: "+appCreated.ClientSecret)
-		panic(err)
-	}
-	b, err = json.Marshal(setupRequestToken{
-		RedirectUris: RedirectUris,
-		ClientID:     appCreated.ClientID,
-		ClientSecret: appCreated.ClientSecret,
-		GrantType:    "authorization_code",
-		Code:         tmpToken,
-	})
-	if err != nil {
-		println("Error", "client_id: "+appCreated.ClientID, " and client_secret: "+appCreated.ClientSecret)
-		panic(err)
-	}
-	req, err = newRequestNoToken(http.MethodPost, utils.GetFullUrl(url, "/oauth/token"), strings.NewReader(string(b)))
-	if err != nil {
-		println("Error", "client_id: "+appCreated.ClientID, " and client_secret: "+appCreated.ClientSecret)
-		panic(err)
-	}
-	res, err = doRequest(req)
-	if err != nil {
-		println("Error", "client_id: "+appCreated.ClientID, " and client_secret: "+appCreated.ClientSecret)
-		panic(err)
-	}
-	b, err = io.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
-	var tokenGot setupTokenGot
-	err = json.Unmarshal(b, &appCreated)
-	if err != nil {
-		println("Error", "client_id: "+appCreated.ClientID, " and client_secret: "+appCreated.ClientSecret)
-		panic(err)
-	}
-	err = VerifyToken(url, tokenGot.AccessToken)
-	if err != nil {
-		println(
-			"Error while verifying the validity of the token",
-			"client_id: "+appCreated.ClientID, " client_secret: "+appCreated.ClientSecret,
-			" token: "+tokenGot.AccessToken,
-		)
-		panic(err)
-	}
-	fmt.Printf("These information are important and sensible. Keep them secret and safe in the right place!\n"+
-		"Your token is: %s\nYour client_id is: %s\nYour client_secret is: %s",
-		tokenGot.AccessToken,
-		appCreated.ClientID,
-		appCreated.ClientSecret,
-	)
+	*clientID = appCreated.ClientID
+	*clientSecret = appCreated.ClientSecret
 }
